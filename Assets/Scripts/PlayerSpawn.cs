ed2c908cb88452cb3079d67ec949b4660ebd03c2
Emily Krohn <emilykrohn2@gmail.com>
2025-10-08 16:18:50 -0700

    Fix Camera Follow
    Camera wasn't following the player. Removed the prefab of the player. Moves the player in the scene instead of spawning a new one in.

--- Assets/Scripts/PlayerSpawn.cs
+++ Assets/Scripts/PlayerSpawn.cs
@@ -6,6 +6,5 @@ public class PlayerSpawn : MonoBehaviour
     public void Spawn(Vector3 playerSpawnPosition)
     {
         gameObject.transform.position = playerSpawnPosition;
-        Instantiate(gameObject);
     }
 }
