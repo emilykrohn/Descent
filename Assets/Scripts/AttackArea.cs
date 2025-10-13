e19ee65e502682b12369e44747bc658845057b02
DylanDaGoat1 <dylandao104@csu.fullerton.edu>
2025-10-13 16:09:22 -0700

    added comments to PA & AA scripts

--- Assets/Scripts/AttackArea.cs
+++ Assets/Scripts/AttackArea.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class AttackArea : MonoBehaviour
 {
-    private int damage = 3;
+    private int damage = 3; // using 3 as an example
 
-    private void OnTriggerEnter2D(Collider2D collider)
+    private void OnTriggerEnter2D(Collider2D collider) //detects whether or not there are colliders inside its trigger area
     {
-        if(collider.GetComponent<Health>() != null)
+        if(collider.GetComponent<Health>() != null) //if they have a health component attached to them
         {
             Health health = collider.GetComponent<Health>();
-            health.Damage(damage);
+            health.Damage(damage); // they will take damage
         }
     }
 }
--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    private GameObject attackArea = default;
+    private GameObject attackArea = default; //the area where the attack happens
 
-    private bool attacking = false;
+    private bool attacking = false; //tracks whether tha player is currently attacking
 
-    private float timeToAttack = 0.25f;
-    private float timer = 0f;
+    private float timeToAttack = 0.25f; //duration of the attack
+    private float timer = 0f; //timer to keep track how long the attack has been active
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,27 +20,27 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space)) //checks if space bar is pressed to trigger an attack
         {
-            Attack();
+            Attack(); //calls attack method
         }
 
         if(attacking)
         {
-            timer += Time.deltaTime;
+            timer += Time.deltaTime; //increases the timer by the time passed since the last frame
 
-            if(timer >= timeToAttack)
+            if(timer >= timeToAttack) //this checks to see if the duration of the attack reached the set time limit
             {
-                timer = 0;
-                attacking = false;
-                attackArea.SetActive(attacking);
+                timer = 0; //resets timer
+                attacking = false; //ends attacking state
+                attackArea.SetActive(attacking); //deactivates the attack area
             }
         }
     }
 
-    private void Attack()
+    private void Attack() //sets attack method to true
     {
         attacking = true;
-        attackArea.SetActive(attacking);
+        attackArea.SetActive(attacking); //activates the attack area
     }
 }
