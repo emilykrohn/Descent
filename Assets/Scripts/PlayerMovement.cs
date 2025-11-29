33cb325930971fb7893030171082ad49b3c1f8aa
Caitlin S. <caitlinsanpedro@gmail.com>
2025-11-29 15:36:32 -0800

    Added Flipping for PlayerMovement
    Flips player when moving left

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,11 +7,17 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     [SerializeField] float speed = 5f;
+
+    bool facingRight = true; // Facing right is true
+
+    private SpriteRenderer spriterenderer; // allows for sprite to flip
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // Used for physics movement
         rb = GetComponent<Rigidbody2D>();
+        spriterenderer = GetComponent<SpriteRenderer>();
     }
 
     // OnMove will be used by the Player Input component when the WASD or Arrow keys are pressed
@@ -19,6 +26,7 @@ public class PlayerMovement : MonoBehaviour
         // Returns a vector with the values -1, 0, or 1 to indicate the direction of movement (x, y)
         Vector2 movement = context.ReadValue<Vector2>();
         rb.linearVelocity = movement * speed;
+        playerFlip(movement.x);
     }
 
     public void SetSpeed(float newSpeed)
@@ -30,4 +38,17 @@ public class PlayerMovement : MonoBehaviour
     {
         return speed;
     }
+
+    private void playerFlip(float movX) // flipping Player when positioned left
+    {
+        if(movX > 0.01f)
+        {
+            spriterenderer.flipX = false; // when facing right
+        }
+        else if(movX < -0.01f)
+        {
+            spriterenderer.flipX = true; // when facing left
+        }
+    }
+
 }
