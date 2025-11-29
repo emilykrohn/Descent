2341d58f354c2591f383b40741070c9a575714c9
emilykrohn <emilykrohn2@gmail.com>
2025-11-29 10:47:09 -0800

    Bullets Damage Enemies

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,11 +4,24 @@ public class Bullet : MonoBehaviour
 {
     float timer = 0f;
     [SerializeField] float cooldown = 2f;
+    [SerializeField] int damage = 3;
+    
     void Update()
     {
         timer += Time.deltaTime;
         if(timer >= cooldown)
         {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Enemy")
+        {
+            Health health = collider.GetComponent<Health>();
+            health.Damage(damage);
+            Debug.Log("Enemy hit by bullet");
             Destroy(gameObject);
         }
     }
