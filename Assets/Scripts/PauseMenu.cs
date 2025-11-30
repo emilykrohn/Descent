d9406a9c86c60baf20989d5adc2a694e12f32ca0
emilykrohn <emilykrohn2@gmail.com>
2025-11-30 09:48:16 -0800

    Start Adding Button Functionality to Pause Menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,11 +4,41 @@ using UnityEngine.UIElements;
 public class PauseMenu : MonoBehaviour
 {
     UIDocument uiDoc;
+    private Button buttonOne;
+    private Button buttonTwo;
+    private Button buttonThree;
+
     void OnEnable()
     {
         // Get the Power Up UI and hide it when the game is first run
         uiDoc = GetComponent<UIDocument>();
         uiDoc.rootVisualElement.style.display = DisplayStyle.None;
+
+        // Find the Buttons from the UIDocument
+        buttonOne = uiDoc.rootVisualElement.Q("ResumeButton") as Button;
+        buttonTwo = uiDoc.rootVisualElement.Q("SaveButton") as Button;
+        buttonThree = uiDoc.rootVisualElement.Q("MainMenuButton") as Button;
+
+        buttonOne.RegisterCallback<ClickEvent>(ResumeGame);
+        buttonTwo.RegisterCallback<ClickEvent>(SaveGame);
+        buttonThree.RegisterCallback<ClickEvent>(ReturnToMainMenu);
+    }
+
+    private void ResumeGame(ClickEvent evt)
+    {
+        CloseUI();
+    }
+
+    private void SaveGame(ClickEvent evt)
+    {
+        Debug.Log("Game Saved!");
+        CloseUI();
+    }
+
+    private void ReturnToMainMenu(ClickEvent evt)
+    {
+        Debug.Log("Returning to Main Menu!");
+        CloseUI();
     }
 
     void Update()
