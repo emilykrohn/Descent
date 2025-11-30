c39550e097a36bcbc5dfe7177a20ccadd453de21
emilykrohn <emilykrohn2@gmail.com>
2025-11-30 12:55:04 -0800

    XP Prefab Moves Toward Player

--- Assets/Scripts/XP.cs
+++ Assets/Scripts/XP.cs
@@ -4,10 +4,16 @@ public class XP : MonoBehaviour
 {
     int xp = 10;
     XpBar xpBar;
+    private Vector3 direction;
+    private Rigidbody2D rb;
+    [SerializeField] private float speed = 3f;
+    private PlayerMovement player;
 
     void Start()
     {
+        player = FindFirstObjectByType<PlayerMovement>();
         xpBar = FindFirstObjectByType<XpBar>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void SetXP(int newXP)
@@ -26,5 +32,12 @@ public class XP : MonoBehaviour
             xpBar.UpdateXp(xp);
             Destroy(gameObject);
         }
+    }
+
+    void Update()
+    {
+        // Move towards the player
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        rb.linearVelocity = new Vector2(direction.x, direction.y) * speed;
     }
 }
