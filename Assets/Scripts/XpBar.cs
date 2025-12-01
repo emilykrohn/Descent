5136620fe5ab5c991d4247e4584ebae321d73f8a
emilykrohn <emilykrohn2@gmail.com>
2025-11-30 19:03:30 -0800

    Increase Amount of XP Needed Each LevelUp

--- Assets/Scripts/XpBar.cs
+++ Assets/Scripts/XpBar.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class XpBar : MonoBehaviour
 {
+    private int startingMaxXp = 100;
     private int maxXp = 100;
     private int currXp;
     private int currLevel = 0;
@@ -23,13 +25,16 @@ public class XpBar : MonoBehaviour
         currXp += amount;
         playerStats.SetXP(currXp);
         // If the current xp is greater than the max xp, increase the players level and open the power up UI
-        if (currXp > maxXp)
+        if (currXp >= maxXp)
         {
             // Decrease current xp by max xp to carry over extra xp to next level
             currXp -= maxXp;
             playerStats.SetXP(currXp);
             currLevel++;
             playerStats.SetCurrentLevel(currLevel);
+            // Increase max xp for next level
+            maxXp = startingMaxXp + (int)Math.Pow(currLevel, 1.8);
+            playerStats.SetMaxXP(maxXp);
             // Update the xp text
             xpText.text = "XP Level: " + currLevel;
             PowerUpUI powerUpUI = FindFirstObjectByType<PowerUpUI>();
